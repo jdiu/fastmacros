60f82f615c8f34ea7447208115b07fc51ef2c63c
fobpholyfe <jasondiu@hotmail.com>
2017-07-01 14:24:11 -0400

    Started building UI

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class TestScreen extends React.Component {
+  static navigationOptions = {
+    title: 'TestScreen',
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text> TestScreen </Text>
+      </View>
+    );
+  }
+}
+
+class LandingScreen extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.mainNav}>
+        <Button 
+          style={styles.mainNavButton}
+          title="Input Macros"
+          onPress={() => navigate('Test')}
+        />
+        <Button 
+          style={styles.mainNavButton}
+          title="Input Calories"
+          onPress={() => navigate('Test')}
+        />
+      </View>
+      
+    );
+  }
+}
+
+const MainNavigation = StackNavigator({
+  Default: { screen: LandingScreen },
+  Test: { screen: TestScreen },
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <MainNavigation />
     );
   }
 }
@@ -16,8 +61,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainNav: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  mainNavButton: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: 50
+  }
 });
