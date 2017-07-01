037c548a2bc193b94f4e8dcdcf6e6383a3555d0b
fobpholyfe <jasondiu@hotmail.com>
2017-07-01 15:40:49 -0400

    Modified UI a bit

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, AppRegistry, Button } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  Button,
+  TouchableHighlight
+} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class TestScreen extends React.Component {
@@ -29,16 +36,17 @@ class LandingScreen extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.mainNav}>
-        <Button 
-          style={styles.mainNavButton}
-          title="Input Macros"
-          onPress={() => navigate('Test')}
-        />
-        <Button 
-          style={styles.mainNavButton}
-          title="Input Calories"
-          onPress={() => navigate('Test')}
-        />
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight style={styles.button} onPress={() => navigate('Test')}>
+            <Text>Input Macros</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={() => navigate('Test')}>
+            <Text>Input Calories</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text>fuck</Text>
+        </View>
       </View>
       
     );
@@ -66,12 +74,25 @@ const styles = StyleSheet.create({
   },
   mainNav: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  mainNavButton: {
+  contentContainer: {
+    flex: 9,
+  },
+  buttonContainer: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center',
-    height: 50
+    alignSelf: 'stretch',
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    justifyContent: 'center'
   }
 });
