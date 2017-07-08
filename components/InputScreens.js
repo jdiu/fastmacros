24768cd4fd4cd225fed0398c7b49adc672335987
fobpholyfe <jasondiu@hotmail.com>
2017-07-08 15:04:15 -0400

    Auto stash before merge of "master" and "origin/master"

--- components/InputScreens.js
+++ components/InputScreens.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import { 
-  StyleSheet,
   Text,
   View,
-  AppRegistry,
-  Button,
-  TouchableHighlight
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView
 } from 'react-native';
 import { globalStyles } from '../globalstyles.js';
 
 export class MacroInputScreen extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+        inputProtein: '0',
+        inputCarbs: '0',
+        inputFats: '0'
+    };
   }
 
   render() {
     return(
-      <View style={ globalStyles.container }>
-        <Text>This is the macro screen</Text>
-      </View>
+        <KeyboardAvoidingView style={ styles.inputWrapper } behavior="padding">
+            <Text>This is the macro screen</Text>
+            
+                <TextInput 
+                    style = {styles.numericInput}
+                    keyboardType = 'numeric'
+                    onChangeText = {(text)=> this.onChanged(text, 'inputProtein')}
+                    value = {this.state.inputProtein}
+                    maxLength = {4}  //setting limit of input
+                />
+                <TextInput 
+                    style = {styles.numericInput}
+                    keyboardType = 'numeric'
+                    onChangeText = {(text)=> this.onChanged(text, 'inputCarbs')}
+                    value = {this.state.inputCarbs}
+                    maxLength = {4}  //setting limit of input
+                />
+                <TextInput 
+                    style = {styles.numericInput}
+                    keyboardType = 'numeric'
+                    onChangeText = {(text)=> this.onChanged(text, 'inputFats')}
+                    value = {this.state.inputFats}
+                    maxLength = {4}  //setting limit of input
+                />
+        </KeyboardAvoidingView>
     );
+  }
+
+  onChanged (text, stateVar) {
+    var reg = new RegExp("^[0-9]+$");
+    if(!reg.test(text)){
+        //set flag for error
+        text = '';
+    };
+    
+    var obj = {};
+    obj[stateVar] = text;
+
+    this.setState(obj);
   }
 }
 
@@ -36,3 +75,15 @@ export class CalorieInputScreen extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+    numericInput: {
+        width: 100,
+        padding: 5,
+    },
+    inputWrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
