f1695aa6bb594a5eb8d2de04c978dae12caca580
Jason Liu <jasonliu0928@gmail.com>
2017-07-08 14:47:09 -0400

    Implemented tabview
    yarn add react-native-tab-view
    Implemented tabviewanimated based on example
    Used footer for generate button

--- App.js
+++ App.js
@@ -10,7 +10,11 @@ import {
 import { StackNavigator } from 'react-navigation';
 import { MacroInputScreen, CalorieInputScreen } from './components/InputScreens';
 import ResultsScreen from './components/ResultsScreen';
-import globalStyles from './globalstyles.js';
+import { globalStyles } from './globalstyles.js';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const MacroRoute = () => <View style={globalStyles.container}><MacroInputScreen /></View>;
+const CalorieRoute = () => <View style={globalStyles.container}><CalorieInputScreen /></View>;
 
 class LandingScreen extends React.Component {
   constructor (props) {
@@ -21,35 +25,69 @@ class LandingScreen extends React.Component {
       protein: .33,
       fat: .33,
       carb: .34,
-      calorieMode: true
-    }
+      calorieMode: true,
+      index: 0,
+      routes: [
+        { key: '1', title: 'Macro Input' },
+        { key: '2', title: 'Calorie Input' },
+      ],
+    };
   }
 
+  _handleChangeTab = index => { 
+    this.setState({ index });
+  };
+  _renderHeader = props => {
+    return (
+      <TabBar 
+        {...props}
+      />
+    );
+  };
+  _renderFooter = props => {
+    return (
+      <Button title='Generate' onPress = {() => this.props.navigation.navigate('Results')}>
+      </Button>
+    )
+  }
+  _renderScene = SceneMap({
+    '1': MacroRoute,
+    '2': CalorieRoute,
+  });
 
   // Use these for optimization (anti-pattern) or nah in touchablehighlights
   // _onPressCal = () => { this.setState({ calorieMode: true }); console.log("cal pressed"); };
 
   render () {
-    const { navigate } = this.props.navigation;
     return (
-      <View style={ styles.mainNav }>
-        <View style={ styles.buttonContainer }>
-          <TouchableHighlight style={ styles.button } onPress={ () => this.setState({ calorieMode: false }) }>
-            <Text>Input Macros</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={ styles.button } onPress={ () => this.setState({ calorieMode: true }) }>
-            <Text>Input Calories</Text>
-          </TouchableHighlight>
-        </View>
-        <View style={ styles.contentContainer }>
-          { this.state.calorieMode ? <CalorieInputScreen /> : <MacroInputScreen /> }
-        </View>
-        <View style={ styles.buttonContainer }>
-          <TouchableHighlight style={ styles.button } onPress = { () => navigate('Results') }>
-            <Text>Next Screen</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
+      // <View style={ styles.mainNav }>
+      //   <View style={ styles.buttonContainer }>
+      //     <TouchableHighlight style={ styles.button } onPress={ () => this.setState({ calorieMode: false }) }>
+      //       <Text>Input Macros</Text>
+      //     </TouchableHighlight>
+      //     <TouchableHighlight style={ styles.button } onPress={ () => this.setState({ calorieMode: true }) }>
+      //       <Text>Input Calories</Text>
+      //     </TouchableHighlight>
+      //   </View>
+      //   <View style={ styles.contentContainer }>
+      //     { this.state.calorieMode ? <CalorieInputScreen /> : <MacroInputScreen /> }
+      //   </View>
+
+      //   <View style={ styles.buttonContainer }>
+      //     <TouchableHighlight style={ styles.button } onPress = { () => navigate('Results') }>
+      //       <Text>Next Screen</Text>
+      //     </TouchableHighlight>
+      //   </View>
+      // </View>
+      <TabViewAnimated
+        style={ styles.tabViewContainer }
+        navigationState={ this.state }
+        renderScene={ this._renderScene }
+        renderHeader={ this._renderHeader }
+        renderFooter={ this._renderFooter }
+        onRequestChangeTab={ this._handleChangeTab }
+      >
+      </TabViewAnimated>
     );
   }
 }
@@ -76,6 +114,9 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 8,
+  },
+  tabViewContainer: {
+    flex: 9
   },
   buttonContainer: {
     flex: 1,
