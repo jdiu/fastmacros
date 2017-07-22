918f069561d16439ea3f361b4456ced5fd0fb001
Jason Liu <jasonliu0928@gmail.com>
2017-07-22 14:02:23 -0400

    updated mcds scrape
    Removes entry if there is no cal/carb/fat/protein
    Outputs to json file

--- dbsrc/mcd_scrape.py
+++ dbsrc/mcd_scrape.py
@@ -1,5 +1,5 @@
 from splinter import Browser
-import re
+import json
 from time import sleep
 from bs4 import BeautifulSoup
 from pathlib import Path
@@ -37,18 +37,33 @@ def open_mcds(filename='site.html'):
 def parse_mcds(file):
     bs = BeautifulSoup(file, 'html.parser')
     menu_coll = bs.find_all('div', attrs={'class': 'menu-collection'})
+    food_list = []
     for menu_c in menu_coll:
         menu_entry = menu_c.find_all('article')
         for ent in menu_entry:
-            name = ent.find('div', attrs={'class': 'menu-item-title'}).find('p').get_text()
+            name = ent.find('div', attrs={'class': 'menu-item-title'}).find('p').get_text().replace('\u00ae', '')
             calories = ent.find('div', attrs={'class': 'menu-item-calories'}).find('p').get_text()
             carbs = ent.find('div', attrs={'class': 'menu-item-carbohydrates'}).find('p').get_text()
             fats = ent.find('div', attrs={'class': 'menu-item-fat'}).find('p').get_text()
             proteins = ent.find('div', attrs={'class': 'menu-item-protein'}).find('p').get_text()
-            print("{}".format(name))
-            print("CAL: {:>4}, CARB: {:>4}, FAT: {:>4}, PROTEIN: {:>4}".format(calories, carbs, fats, proteins))
-            print("------------------------------------")
 
+            if calories == '-' or carbs == '-' or fats == '-' or proteins == '-':
+                continue
+
+            food_list.append({
+                'Name': name,
+                'Calories': calories,
+                'Carbs': carbs,
+                'Fats': fats,
+                'Proteins': proteins,
+                'Restaurant': 'McDonald'
+            })
+
+            # print("{}".format(name))
+            # print("CAL: {:>4}, CARB: {:>4}, FAT: {:>4}, PROTEIN: {:>4}".format(calories, carbs, fats, proteins))
+            # print("------------------------------------")
+    with open('mcds.json', 'w') as fp:
+        json.dump(food_list, fp, indent=4, sort_keys=True)
 
 
 def main():
