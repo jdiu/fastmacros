07f4cdde7745ef7b99d132deaaab16ca1869668a
fobpholyfe <jasondiu@hotmail.com>
2017-10-14 15:59:38 -0400

    setup enum and generate function

--- backend_src/routes/index.js
+++ backend_src/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+var macroSplits = {
+  balanced: {
+    title: "Balanced Option",
+    protein: "40",
+    carbs: "30",
+    fats: "30"
+  },
+  lean: {
+    title: "Lean Option",
+    protein: "40",
+    carbs: "40",
+    fats: "20"
+  },
+  lowCarb: {
+    title: "Low Carb Option",
+    protein:"40",
+    carbs: "20",
+    fats: "40"
+  }
+}
+
+/* for reference */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(generateEntreeCombos(macroSplits.balanced, 2000));
 });
+
+function generateEntreeCombos(macros, cals){
+
+}
+
+
 
 module.exports = router;
